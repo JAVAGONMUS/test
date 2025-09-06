f0c8408880ec1518b82e035e68c3cbc63a485e30
JAVAGONMUS <93622691+JAVAGONMUS@users.noreply.github.com>
2025-09-05 20:43:02 -0600

    quitar boton agregar
            <button onclick="window.open('watch/newpicture.php', '_blank')">AGREGAR</button>

--- index.php
+++ index.php
@@ -31,7 +31,6 @@ if ($archivo_actual == basename($_SERVER["SCRIPT_FILENAME"]) && $archivo_actual 
     <nav>
         <button onclick="window.open('https://www.facebook.com/share/1GrRjVk5LE/', '_blank')">PAGINA EN FACEBOOK</button>
         <button onclick="window.location.href='watch/existencias.php'">CATALOGO GENERAL</button>
-        <button onclick="window.open('watch/newpicture.php', '_blank')">AGREGAR</button>
     </nav>
     
     <main>
